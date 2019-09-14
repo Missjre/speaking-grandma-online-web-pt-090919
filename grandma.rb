cf1c6bc4c9babd49f0b95ae1cbb427e3947a4115
Jalysa Reason <jalysareason@gmail.com>
2019-09-14 00:06:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,3 +13,7 @@ def speak_to_grandma
     puts (speak_to_grandma('Hi!'))= 'HUH?! SPEAK UP, SONNY!')
   end
 
+def speak_to_grandma
+  puts speak_to_grandma{"'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!'")
+    puts (speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!')
+  end
