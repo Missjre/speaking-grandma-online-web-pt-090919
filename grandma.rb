c8c6e7568e05fd9228de27ec7b5f83def5482ffa
Jalysa Reason <jalysareason@gmail.com>
2019-09-14 00:04:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,10 @@ def speak_to_grandma
     puts (speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!')
   end
   
-
+def speak_to_grandma
+  puts speak_to_grandma("'responds with NO, NOT SINCE 1938! when she can hear you'")
+    puts(speak_to_grandma('WHAT DID YOU EAT TODAY?'))= "NO, NOT SINCE 1938!")
+  end
 
 
  def speak_to_grandma("'responds with HUH?! SPEAK UP, SONNY! for a second time'")
